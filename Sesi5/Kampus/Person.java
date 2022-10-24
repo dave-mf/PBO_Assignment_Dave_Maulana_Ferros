0419bb7834491024fd41b43ff969520864934b2b
dave-mf <davemaulanaferros@gmail.com>
2022-10-24 21:01:55 +0700

    Menambahkan Tugas Sesi 5

---
+++ Sesi5/Kampus/Person.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dave Maulana Ferros
+ */
+public class Person {
+    private String name;
+    private String address;
+    
+    public Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public String getAddress(){
+        return this.address;
+    }
+    
+    public void setName(String address){
+        this.address = address;
+    }
+    
+    public String toString(){
+        return  "Nama    : " + getName() +
+        "\nAlamat : " + getAddress();
+    }
+}
